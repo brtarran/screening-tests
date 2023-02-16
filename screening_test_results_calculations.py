e4bca78d019f3e5722b5f8a13d0e88973d7ba142
Brian Tarran <91419526+fatso-jetson@users.noreply.github.com>
2023-02-16 12:31:45 +0000

    code upload

---
+++ screening_test_results_calculations.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 12:52:29 2023
+
+@author: b.tarran
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def calculate_results(sensitivity, specificity, sample_size, prevalence_rate):
+    true_positives = sensitivity * prevalence_rate * sample_size
+    false_positives = (1 - specificity) * (1 - prevalence_rate) * sample_size
+    true_negatives = specificity * (1 - prevalence_rate) * sample_size
+    false_negatives = (1 - sensitivity) * prevalence_rate * sample_size
+    return true_positives, false_positives, true_negatives, false_negatives
+
+def calculate_proportions(true_positives, false_positives, true_negatives, false_negatives, sample_size):
+    true_positives_prop_all = (true_positives / sample_size) * 100
+    false_positives_prop_all = (false_positives / sample_size) * 100
+    true_negatives_prop_all = (true_negatives / sample_size) * 100
+    false_negatives_prop_all = (false_negatives / sample_size) * 100
+    return true_positives_prop_all, false_positives_prop_all, true_negatives_prop_all, false_negatives_prop_all
+
+def calculate_proportions_pos(true_positives, false_positives):
+    true_positives_prop = true_positives / (true_positives + false_positives) * 100
+    false_positives_prop = false_positives / (true_positives + false_positives) * 100
+    return true_positives_prop, false_positives_prop
---
+++ screening_test_results_visualisations.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 12:59:41 2023
+
+@author: b.tarran
+"""
+from screen_tool_vis import *
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Edit these values as needed
+sensitivity = 0.23
+specificity = 0.92
+sample_size = 1000
+prevalence_rate = 0.01
+
+# Calculate TP, FP, TN and FN counts
+(true_positives, false_positives, true_negatives, false_negatives) = calculate_results(
+    sensitivity, specificity, sample_size, prevalence_rate)
+
+# Put into dataframe
+df = pd.DataFrame([[true_positives, false_positives, true_negatives, false_negatives]], columns = (
+    'True positives', 'False positives', 'True negatives', 'False negatives'), index = {'Results'})
+print(df)
+
+# Calculate proportions, put into dataframe
+(true_positives_prop_all, false_positives_prop_all, true_negatives_prop_all,
+ false_negatives_prop_all) = calculate_proportions(
+    true_positives, false_positives, true_negatives, false_negatives, sample_size)
+df2 = pd.DataFrame([[true_positives_prop_all, false_positives_prop_all, 
+                     true_negatives_prop_all, false_negatives_prop_all]], columns = (
+                         'True positives', 'False positives', 'True negatives',
+                         'False negatives'), index = {'Proportion of all tests'})
+print(df2)
+
+# Override style defaults
+my_dpi = 72
+plt.rcParams['figure.figsize'] = ((800/my_dpi), (400/my_dpi))
+plt.rcParams['legend.fontsize'] = 20
+matplotlib.rc('xtick', labelsize=20) 
+matplotlib.rc('ytick', labelsize=20)
+matplotlib.rc('axes', titlesize=20)
+matplotlib.style.use('ggplot')
+
+# Plot Fig1
+df2.plot.barh(stacked = True).legend(loc = 'upper center', ncol = 2)
+plt.yticks([])
+plt.xlim(0, 100)
+plt.xlabel('Test results as % of all tests', fontsize = 20)
+plt.savefig('fig1.png', dpi = my_dpi, bbox_inches = 'tight')
+plt.show()
+
+# Calculate proportion of TP and FP as % of all postive tests, put into dataframe
+(true_positives_prop, false_positives_prop) = calculate_proportions_pos(true_positives,
+                                                                        false_positives)
+df3 = pd.DataFrame([[true_positives_prop, false_positives_prop]], columns = (
+    'True positives', 'False positives'), index = {'Proportion of positive tests'})
+print(df3)
+
+# Plot Fig2
+df3.plot.barh(stacked = True).legend(loc = 'upper center', ncol = 2)
+plt.yticks([])
+plt.xlim(0, 100)
+plt.xlabel('Test results as % of all positive tests', fontsize = 20)
+plt.savefig('fig2.png', dpi = my_dpi, bbox_inches = 'tight')
+plt.show()
+
+# # Calculate proportions and plot
+# df.transpose().apply(lambda x: x*100/sum(x), axis=1).plot(kind="bar", stacked=True)
+# plt.title("Mince Pie Consumption Per Year")
+# plt.xlabel("Year")
+# plt.ylabel("Pies Consumed (%)")
+# plt.show()
+
+
+# print(true_positives, false_positives, true_negatives, false_negatives)
+# print(true_positives_prop_all, false_positives_prop_all, true_negatives_prop_all, false_negatives_prop_all)
+# print(true_positives_prop, false_positives_prop)
+# fig = visualize_results(true_positives_prop_all, false_positives_prop_all, true_negatives_prop_all, false_negatives_prop_all)
+# print(fig)
+# print(true_positives_prop_all + false_positives_prop_all + true_negatives_prop_all + false_negatives_prop_all)
